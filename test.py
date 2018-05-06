2b69f6a416e5f2256316e25abf395115fe05aa66
xzf0217964 <646716148@qq.com>
2018-05-06 10:43:32 +0800

    提交版本-20180506

---
+++ test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3  
+# -*- coding: utf-8 -*-  
+""" 
+ @desc: 
+ @author: ronny 
+ @contact: set@aliyun.com 
+ @site: www.xxxx.com 
+ @software: PyCharm  @since:python 3.6.3 on 2018/05/5 21:20
+ """
+import tensorflow as tf
+
+x = tf.Variable([[0.4, 0.2, 0.4]])
+w = tf.Variable([[-0.5, -0.2], [-0.3, 0.4], [-0.5, 0.2]])
+b = tf.Variable([[0.1, 0.2]])
+xwb = tf.matmul(x, w) + b
+y = tf.nn.relu(tf.matmul(x, w) + b)
+with tf.Session() as sess:
+    init = tf.global_variables_initializer()
+    sess.run(init)
+    print('xwb:', sess.run(xwb))
+    print('y:', sess.run(y))
